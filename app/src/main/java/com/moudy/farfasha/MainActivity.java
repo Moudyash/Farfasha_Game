415b6b08fd33dc2ee2f541de3c360b5681714b49
Moudyash <you@example.com>
2022-06-13 14:53:04 +0300

    Initial commit

---
+++ app/src/main/java/com/moudy/farfasha/MainActivity.java
@@ -0,0 +1,27 @@
+package com.moudy.farfasha;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+ImageButton play;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        play=findViewById(R.id.btnplay);
+play.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        Snackbar.make(findViewById(android.R.id.content), "btn was clicked", Snackbar.LENGTH_LONG).show();
+
+    }
+});
+    }
+}
---
+++ app/src/main/java/com/moudy/farfasha/splash_screen.java
@@ -0,0 +1,25 @@
+package com.moudy.farfasha;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class splash_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(splash_screen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        },3000);
+    }
+}
